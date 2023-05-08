e58bc48925236e89f2dd3bff77ae5d63a504e4b3
labs34 <131676255+labs34@users.noreply.github.com>
2023-05-08 17:17:52 +0530

    Update second.R

--- second.R
+++ second.R
@@ -3,44 +3,45 @@ EmpId <- vector(mode="character", length= n)
 EmpName <- vector(mode="character", length= n)
 DOJ <- vector(mode="character", length= n)
 EmpCode <- vector(mode="numeric",length = n)
+Desig <- vector(mode="character",length = n)
 Dept <- vector(mode="character",length = n)
-Design <- vector(mode="character",length = n)
 
-print("Enter the employee details")
+print("Enter EmpId")
+for (i in 1:n)
+EmpId[i] <- as.character(readline())
+print("Enter EmployeeName")
+for (i in 1:n)
+EmpName [i] <- readline()
+print("Enter DOJ" )
+for (i in 1:n)
+DOJ[i] <- (readline())
+print("Enter EmployeeCode" )
+for (i in 1:n)
+EmpCode[i] <- as.integer(readline())
+print("Enter Designation" )
 
-for (i in 1:n){
- EmpId[i] <- as.character(readline())
- EmpName [i] <-as.character(readline())
- DOJ[i] <- as.character(readline())
- EmpCode[i] <- as.integer(readline())
- Dept[i] <- as.character(readline())
- Design[i] <- as.character(readline())
-}
+for (i in 1:n)
+Desig[i] <- (readline())
+print("Enter Dept" )
+for (i in 1:n)
+Dept[i] <- (readline())
 
-Emp <- data.frame(EmpId,EmpName,DOJ,EmpCode,Dept,Design)
+Emp <- data.frame(EmpId,EmpName,EmpCode,Desig,Dept,DOJ)
 print("The Employee detials are as follows")
 print(Emp)
 
-
 write.csv(Emp,"/home/student/Desktop")
 
-empo<-read.csv("/home/student/Desktop")
-
-print(empo)
+readStudent=read.csv("/home/student/Desktop")
 
 print("Enter a new row")
-a<- readline(prompt = "EmpId")
-b<- readline(prompt = "EmpName")
-c<- readline(prompt = "DOJ")
-d<-readline(prompt = "EmpCode")
-e<- readline(prompt = "Dept")
-f<-  readline(prompt = "Design")
-x<- data.frame(a,b,c,d,e,f)
+u<- readline(prompt = "EmpId")
+  n<- readline(prompt = "EmpName")
+m<- readline(prompt = "EmpCode")
+A<- readline(prompt = "Desig")
+s<- readline(prompt = "Dept")
+t<- readline(prompt = "DOJ")
+x<- data.frame(u,n,m,A,s,t)
 
-print(x)
-
-write.table(x,"/home/student/Desktop",col.names = FALSE, append = TRUE,row.names= TRUE, quote= FALSE, sep = ",")
-
-Emp<-read.csv("/home/student/Desktop")
-print(Emp)
+write.table(x,"/home/student/Desktop",col.names = FALSE, append = T,row.names= T, quote= FALSE, sep = ",")
 
